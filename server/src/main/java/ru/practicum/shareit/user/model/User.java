cce636c31560d845a44e28b8703ebe752bb27fc0
difembaxio <ant.zaharov20177@yandex.ru>
2024-01-20 14:19:41 +0300

    final commit

---
+++ server/src/main/java/ru/practicum/shareit/user/model/User.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.user.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+    @Column(unique = true)
+    private String email;
+}
